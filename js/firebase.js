40f337c76867cc1f888e2f9fada80df28488ec3c
Rebeca Sales <rebecasales@MacBook-Pro-de-Rebeca.local>
2026-06-05 11:19:03 -0300

    config adicionada do firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+  apiKey: "AIzaSyDTAZLnJ4ZnxRbYR-8wgbBQXhZby10Qo4s",
+  authDomain: "mentor-scheduler-demo.firebaseapp.com",
+  projectId: "mentor-scheduler-demo",
+  storageBucket: "mentor-scheduler-demo.firebasestorage.app",
+  messagingSenderId: "483671777537",
+  appId: "1:483671777537:web:25380aa5b479014b4efb00",
+  measurementId: "G-N3R3GRXX04"
 };
 
 export const firebaseEnabled = false;
